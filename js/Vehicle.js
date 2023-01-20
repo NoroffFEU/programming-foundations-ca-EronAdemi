793955439887edb583547b20085538ee34e48cab
EronAdemi <111318711+EronAdemi@users.noreply.github.com>
2023-01-20 23:12:25 +0100

    Added more information

---
+++ js/Vehicle.js
@@ -0,0 +1,11 @@
+const vehicle = {
+    make: "Volvo",
+    model: "S90",
+    year: 2020,
+    color: "black",
+    hasSunroof: true,
+    features: ["leather seats", "navigation system", "park assist"],
+    honk: function() {
+      return "Honk Honk!";
+    }
+}
